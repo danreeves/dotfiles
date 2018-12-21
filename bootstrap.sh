df91c8f0e5ef838ee85d94e54833e73b564d38b9
Dan Reeves <hey@danreev.es>
2018-12-21 22:55:18 +0000

    finish bootstrap script?

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,15 @@
 for file in files/*
 do
   if [ -f "$file" ]; then
-	  echo "Linking $(pwd)/$file to ~/.$(basename $file)"
-	  ln -s $(pwd)/$file ~/.$(basename $file)
+    echo "Linking $(pwd)/$file to ~/.$(basename $file)"
+    ln -sfn $(pwd)/$file ~/.$(basename $file)
+  else
+    echo "Creating ~/.$(basename $file)/"
+    mkdir -p ~/.$(basename $file)
+    for dirfile in $file/*
+    do
+      echo "Linking $(pwd)/$dirfile to ~/.$(basename $file)/$(basename $dirfile)"
+      ln -sfn $(pwd)/$dirfile ~/.$(basename $file)/$(basename $dirfile)
+    done
   fi
 done
