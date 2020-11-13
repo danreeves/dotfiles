10c90594553cd9ac61c49fccc66a05d6aeea1ff9
Dan Reeves <hey@danreev.es>
2020-11-13 19:09:27 +0000

    no-optional-locks to prevent tmux git status inteferring with git usage

--- files/zshfns/gitbranchname.sh
+++ files/zshfns/gitbranchname.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh -f
 
-branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
-gitstatus=$(git status --porcelain 2> /dev/null)
+branch=$(git --no-optional-locks rev-parse --abbrev-ref HEAD 2> /dev/null)
+gitstatus=$(git --no-optional-locks status --porcelain 2> /dev/null)
 dirty=$([[ $gitstatus == "?? 0" || $gitstatus == "" ]] && echo "" || echo " *")
 if [[ $branch != "" ]]; then
   echo " | $branch$dirty";
