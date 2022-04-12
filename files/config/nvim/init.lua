e7e1a2edd91f14005550dcdba9636de51102a654
Dan Reeves <hey@danreev.es>
2022-04-12 01:10:22 +0100

    lua lang server

--- files/config/nvim/init.lua
+++ files/config/nvim/init.lua
@@ -191,6 +191,36 @@ for _, lsp in pairs(servers) do
 	})
 end
 
+local runtime_path = vim.split(package.path, ";")
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+require("lspconfig").sumneko_lua.setup({
+	settings = {
+		Lua = {
+			single_file_support = true,
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+				-- Setup your lua path
+				path = runtime_path,
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim" },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+})
+
 require("telescope").load_extension("fzf")
 local actions = require("telescope.actions")
 require("telescope").setup({
