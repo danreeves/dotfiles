c18b5a4909a31019199772f653ded15f2b967ccc
Dan Reeves <hey@danreev.es>
2022-05-19 16:18:07 +0100

    longer git branch name in tmux

--- files/zshfns/gitbranchname.sh
+++ files/zshfns/gitbranchname.sh
@@ -4,7 +4,7 @@ branch=$(git --no-optional-locks rev-parse --abbrev-ref HEAD 2> /dev/null)
 gitstatus=$(git --no-optional-locks status --porcelain 2> /dev/null)
 dirty=$([[ $gitstatus == "?? 0" || $gitstatus == "" ]] && echo "" || echo " *")
 if [[ $branch != "" ]]; then
-  echo " | ${branch:0:8}$dirty";
+  echo " | ${branch:0:9}$dirty";
 fi
 
 tmux rename-session "${PWD##*/}"
